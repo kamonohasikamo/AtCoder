a185ff9f5066789588eb1712138262a83611ee7c
kamonohasikamo <kamonohasikamo@users.noreply.github.com>
2019-05-15 14:53:38 +0900

    むかちゃっかふぁいやー

---
+++ 107-b.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <limits.h>
+
+#define ROOP(i,n) for(i=0;i<n;i++)
+
+//prototype declaration
+int getint();
+long long getllint();
+double getdint();
+char getch();
+void setstring(char *s);
+void showint(int i);
+void showllint(long long i);
+void showdint(double i);
+void showchar(char c);
+void showstring(char *s);
+
+int myPower(int a, int n) { // aのn乗の計算
+	int x = 1;
+	while (n > 0) { // 全てのbitが捨てられるまで
+		if ( n & 1) { // 一番右のbitが１のとき
+			x *= a;
+		}
+		a *= a;
+		n >>= 1; // bit全体を右に1つシフトして一番右を捨てる
+	}
+	return x;
+}
+
+int main(void) {
+	int H, W, i = 0, j = 0, k = 0;
+	H = getint();
+	W = getint();
+	char a[H][W];
+	ROOP(i, H) {
+			scanf("%s", a[i]);
+	}
+	int l = 0, c = 0;
+	ROOP(i, H) {
+		l = 0;
+		ROOP(j, W) {
+			c = 0;
+			ROOP(k, H) {
+				if (a[k][j] == '#') {
+					c++;
+					break;
+				}
+			}
+			ROOP(k, W) {
+				if (a[i][k] == '#') {
+					c++;
+					break;
+				}
+			}
+			if (c == 2) {
+				printf("%c", a[i][j]);
+				l++;
+			}
+		}
+		if (l != 0) {
+			printf("\n");
+		}
+	}
+	return 0;
+}
+
+int getint() {
+	int i;
+	scanf("%d", &i);
+	return i;
+}
+
+long long getllint() {
+	long long i;
+	scanf("%lld", &i);
+	return i;
+}
+
+double getdint() {
+	double i;
+	scanf("%lf", &i);
+	return i;
+}
+
+char getch() {
+	char c;
+	scanf("%c", &c);
+	return c;
+}
+
+void setstring(char *s) {
+	scanf("%s", s);
+}
+
+void showint(int i) {
+	printf("%d\n", i);
+}
+
+void showllint(long long i) {
+	printf("%lld\n", i);
+}
+
+void showdint(double i) {
+	printf("%lf\n", i);
+}
+
+void showchar(char c) {
+	printf("%c\n", c);
+}
+
+void showstring(char *s) {
+	printf("%s\n", s);
+}
