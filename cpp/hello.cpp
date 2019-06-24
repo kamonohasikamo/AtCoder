ae836b0ffc03d1d72af21ac616786a24afcc4bae
kamonohasikamo <kamonohasikamo@users.noreply.github.com>
2019-06-24 09:35:50 +0900

    C++はじめました

---
+++ cpp/hello.cpp
@@ -0,0 +1,19 @@
+//Hello C++
+#include <iostream>
+//#include <string>
+using namespace std;
+
+void func();
+
+void func() {
+	cout << "Hello world!\n";
+}
+
+int main() {
+// 	int i, j;
+// 	cout << "This program is add.\n 2 value here!\n";
+// 	cin >> i >> j;
+// 	cout << "2 value add resule : " << i + j;
+	func();
+	return 0;
+}
